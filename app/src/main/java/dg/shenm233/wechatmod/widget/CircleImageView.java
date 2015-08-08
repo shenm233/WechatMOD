f1fc3947e48f65e5c23e9bf8e2a295926f8ece28
shenm233 <darkgenlotus@gmail.com>
2015-08-08 15:28:06 +0800

    Update CircleImageView maybe fix crash

--- app/src/main/java/dg/shenm233/wechatmod/widget/CircleImageView.java
+++ app/src/main/java/dg/shenm233/wechatmod/widget/CircleImageView.java
@@ -12,9 +12,9 @@ import android.widget.ImageView;
 
 public class CircleImageView extends ImageView {
     private static Bitmap mBitmap;
-    private static Canvas mCanvas;
-    private final Paint mBitmapPaint = new Paint();
-    private BitmapShader mBitmapShader;
+    private static Canvas mCanvas = new Canvas();
+    private final static Paint mBitmapPaint = new Paint();
+    private static BitmapShader mBitmapShader;
     private static float radius;
 
     public CircleImageView(Context context) {
@@ -33,23 +33,27 @@ public class CircleImageView extends ImageView {
     public void onDraw(Canvas imageViewCanvas) {
         //Hack for wechat!
         try {
+            final int mMeasuredWidth = getMeasuredWidth();
+            final int mMeasuredHeight = getMeasuredHeight();
+
+            if (mMeasuredWidth <= 0 || mMeasuredHeight <= 0) {
+                Log.d("WechatMOD", "getMeasuredWidth() or getMeasuredHeight() <= 0  stop drawing!");
+                return;
+            }
+
+            if (mBitmap == null) {
+                mBitmap = Bitmap.createBitmap(mMeasuredWidth, mMeasuredHeight, Bitmap.Config.ARGB_8888);
+                Log.d("WechatMOD", "created bitmap! width=" + mMeasuredWidth + " height=" + mMeasuredHeight);
+                mCanvas.setBitmap(mBitmap);
+                radius = Math.min(mMeasuredWidth, mMeasuredHeight) / 2;
+                mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                mBitmapPaint.setAntiAlias(true);
+                mBitmapPaint.setShader(mBitmapShader);
+            }
+
             //draw bitmap on canvas for getting another bitmap
             super.onDraw(mCanvas);
 
-            if (mBitmap == null) {
-                mBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-                mCanvas = new Canvas(mBitmap);
-                radius = Math.min(getWidth(), getHeight()) / 2;
-            }
-
-            //clear
-            mCanvas.drawColor(0xffffff);
-            mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-            mBitmapPaint.setAntiAlias(true);
-            mBitmapPaint.setShader(mBitmapShader);
-
-            //real drawing on ImageView
-            imageViewCanvas.drawColor(0xffffff);
             imageViewCanvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mBitmapPaint);
 
             Log.d("WechatMOD", "draw Circle!");
